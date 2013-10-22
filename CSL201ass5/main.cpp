b52345de890174120fac573dedc065d1ed1ad3f5
codeofRobin <malhotra.robin@icloud.com>
2013-10-23 01:19:54 +0530

    Initial Commit

---
+++ CSL201ass5/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CSL201ass5
+//
+//  Created by Robin Malhotra on 23/10/13.
+//  Copyright (c) 2013 Robin's code kitchen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
